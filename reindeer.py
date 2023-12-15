d50cac8659e571cd649e1a7a1a3e9badc817b8e7
lexij07 <145355812+lexij07@users.noreply.github.com>
2023-12-15 09:22:01 -0500

    Update reindeer.py

--- reindeer.py
+++ reindeer.py
@@ -1,3 +1,14 @@
-# Programmer: 
-# Description: 
+# Programmer: Lexi
+# Description: Santas Reindeer
 
+# A list of a student's courses
+reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donder", "Blitzen"]
+
+
+# Print the student's timetable, one course per line
+print("Reindeer:")
+reindeer_num = 1
+for name in reindeer:
+    print(f"{name}")
+    reindeer_num += 1
+    
